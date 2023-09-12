1147b3883511315bb771719557608eb2bccf1275
aidenk1 <aidenhw.kim@gmail.com>
2023-09-12 08:52:32 -0700

    d

---
+++ _posts/asdasd.py
@@ -0,0 +1,13 @@
+metadata = {
+    "toc": True,
+    "comments": False,
+    "layout": "post",
+    "title": "Python Quiz",
+    "description": "Math Quiz",
+    "type": "hacks",
+    "courses": {
+        "compsci": {
+            "week": 3
+        }
+    }
+}
